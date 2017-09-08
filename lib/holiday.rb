8ecb0eed519006eb85668160da410c318b9e4d0b
Chris Nady <nady.christopher@gmail.com>
2017-09-08 01:36:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,9 +103,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_names =[]
   holiday_hash.each do |key, value|
     value.each do |key, value|
-      value.include?("BBQ")
+      if value.include?("BBQ")
+        holiday_names.push(key)
+      end
     end
   end
 
