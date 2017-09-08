18e0a82c2a4f9dd89713bbb37fd1b0c9f69ffc70
Chris Nady <nady.christopher@gmail.com>
2017-09-08 01:12:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
             new_string<<"#{supply}, "
           else
             new_string<<"#{supply}"
+          end
         end
       puts "#{holiday.to_s.capitalize!}: #{new_string}"
     end
